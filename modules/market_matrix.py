b9468ab15bac94e59307ce1cc5dc598e6c4b7e24
alvafir <alvafir@gmail.com>
2026-07-25 19:39:14 -0400

    Delete modules directory

--- modules/market_matrix.py
+++
@@ -1,2 +0,0 @@
-DISPLAY_COLUMNS=["Mercado","Local casa %","Visitante fuera %","Probabilidad %","Confianza JOYA","Tier","Riesgo","Calidad","Consistencia","Muestra"]
-def get_group_table(t,g):return t[t["Grupo"]==g][DISPLAY_COLUMNS].copy() if not t.empty else t
